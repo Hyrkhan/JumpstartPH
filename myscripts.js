82388add65f3499fdc425182a1d9907ef62d69d9
Hyrkan <109449003+Hyrkhan@users.noreply.github.com>
2023-11-29 12:17:52 +0000

    making validations for registration

--- myscripts.js
+++ myscripts.js
@@ -2,7 +2,8 @@
 function validateForm(event) {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-
+    var phoneNumber = document.getElementById('phonenumber').value;
+    
     // validate email if correct
     if (!isValidEmail(email)) {
         displayModal('Please enter a valid email address.');
@@ -19,6 +20,20 @@ function validateForm(event) {
 
     return true; // if all true, allow submit
 }
+
+// function that checks the phone number and validates it
+function validateForm2(event) {
+    var phoneNumber = document.getElementById('phonenumber').value;
+
+    if (!isValidPhoneNumber(phoneNumber)) {
+        displayModal('Phone number must be 11 digits long and in "09XX-XXX-XXXX" format.');
+        event.preventDefault(); // prevent form submission
+        return false;
+    }
+    
+    return true; // if true, allow submit
+}
+
 // function that display the message on the div
 function displayModal(message) {
     var modal = document.getElementById('validationModal');
@@ -26,13 +41,21 @@ function displayModal(message) {
     modalContent.innerHTML = message;
     modal.style.display = 'block';
 }
+
 // function that closes the div
 function closeModal() {
     var modal = document.getElementById('validationModal');
     modal.style.display = 'none';
 }
+
 // function that validates the email entered
 function isValidEmail(email) {
     // validate email using RegEx
     return /\S+@\S+\.\S+/.test(email);
+}
+
+// function that validates the phone number entered
+function isValidPhoneNumber(phoneNumber) {
+    // validate phone number using RegEx
+    return /09\d{2}-\d{3}-\d{4}/.test(phoneNumber);
 }
